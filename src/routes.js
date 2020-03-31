03ba262bd75450a353efd07944ccd8a673d0836e
Thiago Costa <thiagonsc.costa@hotmail.com>
2020-03-31 03:54:55 -0300

    backend express

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionsController = require('./controllers/SessionsController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionsController.create);
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.list);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.list);
+routes.delete('/incidents:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
